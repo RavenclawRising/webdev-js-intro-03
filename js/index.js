4c43bb49a3a251abdfeb5a1b30705884782de854
unknown <Lauradu@me.com>
2025-01-20 03:12:45 -0500

    Modified index.html, js/index.js, and README.md. Added answers.txt.

--- js/index.js
+++ js/index.js
@@ -2,3 +2,19 @@
 // that are not declared
 "use strict"
 
+/* 
+
+console.log(12 * 3); //Output: 36
+console.log(12 / 3); //Output: 4
+console.log(12 % 2); //Output: 0
+console.log(11 % 2); //Output: 1
+
+console.log(3**3); //Output: 27
+console.log(7**7); //Output: 49
+console.log(9**13); //Output: 2541865828329
+console.log(95871 % 12); //Output 3
+
+console.log((1 + 1) === (1 + "1"));   //Output: false
+
+*/
+
